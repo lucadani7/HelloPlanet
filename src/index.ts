6abb6d59445c951254b89f5bc112c07996ee2bfa
lucadani7 <79052990+lucadani7@users.noreply.github.com>
2025-07-11 01:14:02 +0300

    index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { PlanetsManager } from "./PlanetsManager";
 export { planetDistancesAU } from "./PlanetDistance";
 export { scenes } from "./scenes";
+export { default as EnumPlanets } from "./EnumPlanets";
+export { default as PlanetInfo } from "./PlanetInfo";
